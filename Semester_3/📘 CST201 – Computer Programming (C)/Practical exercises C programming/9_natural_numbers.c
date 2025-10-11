fa43dd8acc39b4d5d2a28ea4fde9ee543e59ab3e
sagar900aj <sagar900aj@gmail.com>
2025-10-11 13:35:58 +0530

    Create 9_natural_numbers.c

---
+++ Semester_3/📘 CST201 – Computer Programming (C)/Practical exercises C programming/9_natural_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, i = 1;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    for (i; i <= n; i++)
+    {
+        printf("%d\n", i);
+    }
+return 0;
+}
